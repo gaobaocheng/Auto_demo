bc4af6f6f31588d0b618b95aa9925c1df14cfff0
gaobaocheng-pc <18356620096@163.com>
2021-05-05 18:09:12 +0800

    第一次整合提交github测试

---
+++ src/com/test/java/day01/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.test.java.day01;
+
+
+public class HelloWorld {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 4;
+        System.out.println(a + b);
+
+
+
+
+    }
+
+}
